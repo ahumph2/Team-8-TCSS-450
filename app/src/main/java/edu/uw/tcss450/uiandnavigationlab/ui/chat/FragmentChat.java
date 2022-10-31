f12c9278da87f4384f7108645cbd0d075d47878e
Alex <ahumph2@uw.edu>
2022-10-31 13:26:52 -0700

    Name changes and moved files for weather and chat fragments

--- app/src/main/java/edu/uw/tcss450/uiandnavigationlab/ui/chat/FragmentChat.java
+++ app/src/main/java/edu/uw/tcss450/uiandnavigationlab/ui/chat/FragmentChat.java
@@ -1,4 +1,4 @@
-package edu.uw.tcss450.uiandnavigationlab;
+package edu.uw.tcss450.uiandnavigationlab.ui.chat;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import edu.uw.tcss450.uiandnavigationlab.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Fragment_weather#newInstance} factory method to
+ * Use the {@link FragmentChat#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Fragment_weather extends Fragment {
+public class FragmentChat extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class Fragment_weather extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Fragment_weather() {
+    public FragmentChat() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class Fragment_weather extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment_weather.
+     * @return A new instance of fragment fragment_chat.
      */
     // TODO: Rename and change types and number of parameters
-    public static Fragment_weather newInstance(String param1, String param2) {
-        Fragment_weather fragment = new Fragment_weather();
+    public static FragmentChat newInstance(String param1, String param2) {
+        FragmentChat fragment = new FragmentChat();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class Fragment_weather extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weather, container, false);
+        return inflater.inflate(R.layout.fragment_chat, container, false);
     }
 }
--- app/src/main/java/edu/uw/tcss450/uiandnavigationlab/ui/weather/FragmentWeather.java
+++ app/src/main/java/edu/uw/tcss450/uiandnavigationlab/ui/weather/FragmentWeather.java
@@ -1,4 +1,4 @@
-package edu.uw.tcss450.uiandnavigationlab;
+package edu.uw.tcss450.uiandnavigationlab.ui.weather;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import edu.uw.tcss450.uiandnavigationlab.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link fragment_chat#newInstance} factory method to
+ * Use the {@link FragmentWeather#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class fragment_chat extends Fragment {
+public class FragmentWeather extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class fragment_chat extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public fragment_chat() {
+    public FragmentWeather() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class fragment_chat extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_chat.
+     * @return A new instance of fragment Fragment_weather.
      */
     // TODO: Rename and change types and number of parameters
-    public static fragment_chat newInstance(String param1, String param2) {
-        fragment_chat fragment = new fragment_chat();
+    public static FragmentWeather newInstance(String param1, String param2) {
+        FragmentWeather fragment = new FragmentWeather();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class fragment_chat extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        return inflater.inflate(R.layout.fragment_weather, container, false);
     }
 }
