7c4bc6daadfe1276132906765f5d5dfea5858b85
Elijah Amian <elijah25@uw.edu>
2022-10-31 17:32:19 -0700

    Implement Chat App

--- app/src/main/java/edu/uw/tcss450/uiandnavigationlab/ui/chat/ChatFragment.java
+++ app/src/main/java/edu/uw/tcss450/uiandnavigationlab/ui/chat/ChatFragment.java
@@ -25,7 +25,7 @@ public class ChatFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        return inflater.inflate(R.layout.fragment_chat_list, container, false);
     }
 
     @Override
